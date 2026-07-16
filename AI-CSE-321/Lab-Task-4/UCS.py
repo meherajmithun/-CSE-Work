d47f3dc237246f16081e4b3f6d3a1aa8a2445fbd
meherajmithun <meherajhossainmithun@gmail.com>
2026-07-17 01:13:58 +0600

    add ucs

--- AI-CSE-321/Lab-Task-4/UCS.py
+++ AI-CSE-321/Lab-Task-4/UCS.py
@@ -1,3 +1,5 @@
+import heapq
+
 graph = {'A': [('B',5), ('C', 7)],
          'B': [('D', 7),('E', 3)],
          'C': [('F', 4)],
@@ -5,3 +7,30 @@ graph = {'A': [('B',5), ('C', 7)],
          'E': [],
          'F': []
          }
+
+def ucs(src, dest):
+    pq = []
+    heapq.heappush(pq, (0, src, [src]))
+    visited=[]
+    while pq:
+        cost,node,path = heapq.heappop(pq)
+        if node in visited:
+            continue
+        
+        visited.append(node)
+        print(node, end=" ")
+        if node == dest:
+            print(" -> Goal Reached")
+            # print(path); 
+            print("path : "," -> ".join(path))
+            print("Total cost : ", cost)
+            return;
+
+        for neighbour, weight in graph[node]:
+            if neighbour in visited:
+                continue
+            heapq.heappush(pq, (weight+cost, neighbour, path+[neighbour]))
+
+    print("Goal Not Found")
+
+ucs('A', 'F')
